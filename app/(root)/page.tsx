4a4746fdca06723b9a3770a918a55ebb3f77b6db
aagra109 <aagra109@asu.edu>
2024-07-14 15:51:06 -0700

    implement left sidebar and mobile navbar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,33 @@
+import TotalBalanceBox from '@/components/TotalBalanceBox'
+import HeaderBox from '@/components/ui/HeaderBox'
+import { Fira_Sans } from 'next/font/google'
 import React from 'react'
 
-const page = () => {
+const Home = () => {
+  const loggedIn ={ firstName: 'John'}
   return (
-    <div>Home</div>
+    <section className='home'>
+        <div className='home-content'>
+            <header className='home-header'>
+            <HeaderBox 
+                type="greeting"
+                title="Welcome"
+                user= {loggedIn?.firstName || 'Guest'}
+                subtext="Access and manage your account 
+                and transactions efficiently"
+                />
+            
+            <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.25}
+            />
+            </header>
+            RECENT TRANSACTIONS
+        </div>
+        RIGHT SIDEBAR
+    </section>
   )
 }
 
-export default page
+export default Home
